aa82a5e8ccc319fb7303c94211584974ab99e798
Ankeet <postmillenial@gmail.com>
2016-04-07 14:23:56 -0400

    no changes?

--- http_speed.py
+++ http_speed.py
@@ -16,7 +16,24 @@ A check the speed of an HTTP request!
 
 ###############################'''
 
-import cProfile
+import time
+
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000 
+        if self.verbose: 
+            print 'elapsed time: %f ms' % self.msecs
+
+
 
 def main():
     
@@ -24,4 +41,7 @@ def main():
     a = requests.get("http://httpbin.org/delay/9")
 
 if  __name__ == '__main__':
-    cProfile.run('main()')
+    with Timer() as t:
+        main()
+     print "=> Execution time of main: %s s" t.secs
+
